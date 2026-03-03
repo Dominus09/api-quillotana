21df640125b1041a9b25115854d73871f013fe44
Dominus09 <katoonxd@gmail.com>
2026-03-03 16:52:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,23 @@ app.get("/debug-variant", async (req, res) => {
   }
 });
 
+app.get("/debug-stock", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.bsale.io/v1/stocks.json?limit=1&offset=0",
+      {
+        headers: {
+          access_token: process.env.BSALE_TOKEN,
+        },
+      }
+    );
+
+    res.json(response.data.items[0]);
+  } catch (error) {
+    res.status(500).json({ error: "Error stock debug", detail: error.message });
+  }
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`API corriendo en puerto ${PORT}`);
 });
